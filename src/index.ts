6aca83cb58e8c23f7b03f2e18cab7420ff677177
DamianToromanovic <d.toromanovic@web.de>
2025-06-21 21:32:07 +0200

    index.ts and dev dependecnies installed

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (_, res) => {
+  res.send("Hello from project-pilot backend!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});

c0d5389b1147dd87d04252f14af45dd762015af8
DamianToromanovic <d.toromanovic@web.de>
2025-06-25 12:18:26 +0200

    exported prisma => just one generator

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,12 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route";
-import { PrismaClient } from "../src/generated/prisma";
-
-const prisma = new PrismaClient();
+import prisma from "./lib/prisma";
 
 dotenv.config();
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 

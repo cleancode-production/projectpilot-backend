8471514e392bc6f33605a61f6a459c0843ba727b
DamianToromanovic <d.toromanovic@web.de>
2025-06-30 14:22:41 +0200

    task routes and controller updfated

--- src/controllers/task.controller.ts
+++ src/controllers/task.controller.ts
@@ -115,3 +115,120 @@ export const getTasksByProject = async (req: Request, res: Response) => {
     return;
   }
 };
+
+export const getTaskById = async (req: Request, res: Response) => {
+  const userId = req.user?.userId;
+  const taskId = req.params.id;
+
+  if (!userId) {
+    res.status(401).json({ message: "Invalid token" });
+    return;
+  }
+
+  if (!taskId) {
+    res.status(400).json({ message: "Task ID is required" });
+    return;
+  }
+  try {
+    // 1. Task holen
+    const task = await prisma.task.findUnique({
+      where: { id: taskId },
+      include: {
+        project: {
+          select: { workspaceId: true },
+        },
+      },
+    });
+
+    if (!task) {
+      res.status(404).json({ message: "Task not found" });
+      return;
+    }
+
+    // 2. Prüfen, ob User Mitglied im Workspace ist
+    const isMember = await prisma.workspaceMember.findFirst({
+      where: {
+        userId,
+        workspaceId: task.project.workspaceId,
+      },
+    });
+
+    if (!isMember) {
+      res.status(403).json({ message: "Access denied" });
+      return;
+    }
+
+    res.status(200).json(task);
+  } catch (error) {
+    console.error("Error in getTaskById:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const updateTask = async (req: Request, res: Response) => {
+  const userId = req.user?.userId;
+
+  const taskId = req.params.id;
+
+  const {
+    title,
+    description,
+    dueDate,
+    priority,
+    status,
+    assignedTo,
+    tags,
+    comments,
+  } = req.body;
+
+  if (!userId) {
+    res.status(401).json({ message: "invalid Token" });
+    return;
+  }
+
+  if (!taskId) {
+    res.status(400).json({ message: "taskID is required" });
+    return;
+  }
+
+  const task = await prisma.task.findUnique({
+    where: { id: taskId },
+    include: {
+      project: {
+        select: { workspaceId: true },
+      },
+    },
+  });
+
+  if (!task) {
+    res.status(404).json({ message: "Task not found" });
+    return;
+  }
+
+  const isMember = await prisma.workspaceMember.findFirst({
+    where: {
+      id: userId,
+      workspaceId: task?.project.workspaceId,
+    },
+  });
+
+  if (!isMember) {
+    res.status(403).json({ message: "Access denied" });
+    return;
+  }
+
+  const dataToUpdate: Record<string, any> = {};
+  if (title) dataToUpdate.title = title;
+  if (description) dataToUpdate.description = description;
+  if (dueDate) dataToUpdate.dueDate = new Date(dueDate);
+  if (priority) dataToUpdate.priority = priority;
+  if (status) dataToUpdate.status = status;
+  if (assignedTo) dataToUpdate.assignedTo = assignedTo;
+  if (tags) dataToUpdate.tags = tags;
+  if (comments) dataToUpdate.comments = comments;
+
+  const updatedTask = await prisma.task.update({
+    where: { id: taskId },
+    data: dataToUpdate,
+  });
+};
--- src/routes/project.route.ts
+++ src/routes/project.route.ts
@@ -7,6 +7,8 @@ import {
   updateProject,
 } from "../controllers/project.controller";
 
+import { createTask, getTasksByProject } from "../controllers/task.controller";
+
 import { verifyToken } from "../middleware/auth.middleware";
 
 const router = Router();
@@ -16,5 +18,8 @@ router.post("/", verifyToken, createProject);
 router.get("/:id", verifyToken, getProjectById);
 router.delete("/:id", verifyToken, deleteProjectById);
 router.patch("/:id", verifyToken, updateProject);
+
+router.post("/:id/tasks", verifyToken, createTask);
+router.get("/:id/tasks", verifyToken, getTasksByProject);
 
 export default router;
